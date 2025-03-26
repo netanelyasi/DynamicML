ba875eefcf99643535de6f77e93322e3651ca910
netanelyasi <126358650+netanelyasi@users.noreply.github.com>
2025-03-26 08:08:37 +0200

    Create init.py

---
+++ app/init.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+import logging
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+logger = logging.getLogger(Path(__file__).name)
